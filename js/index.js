ef139c3051c954a87a081da600c33aa7e6dea7ea
ryskiz <ryan.skinner1193@gmail.com>
2017-02-01 02:40:58 -0500

    soundcloud up and running

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $(window).scroll(function() {
         if ($(this).scrollTop() > 30){
@@ -11,19 +12,29 @@ $(document).ready(function(){
             $('#brand').slideDown();
         }
     });
-    // $('h1').on('click', function(){
-    //     $(this).fadeOut("slow", function(){
-    //
-    //     });
-    //     $(this).fadeIn("slow", function(){
-    //
-    //     })
-    // })
     $('.album-photo').on('mouseenter', function(){
         $(this).addClass('blur');
+        $('h1').fadeIn();
     });
     $('.album-photo').on('mouseleave', function(){
         $(this).removeClass('blur');
+        $('h1').fadeOut();
     });
-    
 });
+let widgetIframe = document.getElementById('sc-widget'),
+    widget = SC.Widget(widgetIframe);
+widget.bind(SC.Widget.Events.READY, function() {
+    widget.bind(SC.Widget.Events.PLAY, function() {
+        // get information about currently playing sound
+        widget.getCurrentSound(function(currentSound) {
+            console.log('sound ' + currentSound.get('') + 'began to play');
+        });
+    });
+    // get current level of volume
+    widget.getVolume(function(volume) {
+        console.log('current volume value is ' + volume);
+    });
+    // set new volume level
+    widget.setVolume(50);
+    // get the value of the current position
+})();
