c2c2ab2da9522409561e9c2c52d4fa43fcc03421
ryskiz <ryan.skinner1193@gmail.com>
2017-01-31 21:20:23 -0500

    navbar animations

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-function hi() {}
+
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50){
+            $('header').addClass("sticky");
+            $('img').animate({
+                "height": "80px"
+            }).next().slideDown();
+        }
+        else {
+            $('header').removeClass("sticky");
+        }
+    });
+    $('h1').on('click', function(){
+        $(this).fadeOut("slow", function(){
+
+        });
+        $(this).fadeIn("slow", function(){
+            
+        })
+    })
+    
+});
